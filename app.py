f2776d78db95db6ba33c3546617cb77af372e2d2
Sergio Peralta <seperalta88@gmail.com>
2023-10-29 20:44:19 -0300

    Add PDF processing functionality to sidebar

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
+from dotenv import load_dotenv
 
 
 def main():
+    load_dotenv()
     st.set_page_config(page_title="Chat with your documents", page_icon="📝")
 
     st.header("Chat with your documents")
@@ -11,8 +13,16 @@ def main():
 
     with st.sidebar:
         st.subheader("Your documents")
-        st.file_uploader("Upload your PDFs here and click on 'Process'")
-        st.button("Process")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True
+        )
+        if st.button("Process"):
+            with st.spinner("Processing your documents..."):
+            # get pdf text
+
+            # get pdf embeddings
+
+            # create vector store
 
 
 if __name__ == "__main__":
