4dc1d35c5daacbac8a6ab563a12dc216bc2651fb
Sergio Peralta <seperalta88@gmail.com>
2023-10-29 20:56:17 -0300

    Add function to extract text from PDF documents

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import streamlit as st
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)  # read pdf
+        for page in pdf_reader.pages:  # iterate over pages
+            text += page.extract_text()  # extract text
+    return text  # return text
 
 
 def main():
@@ -18,7 +28,10 @@ def main():
         )
         if st.button("Process"):
             with st.spinner("Processing your documents..."):
-            # get pdf text
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+
+                st.write(raw_text)
 
             # get pdf embeddings
 
