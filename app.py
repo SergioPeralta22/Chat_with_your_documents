b8e8ba0dbd213722e6526935a1c548208eaf15f2
Sergio Peralta <seperalta88@gmail.com>
2023-10-29 23:23:11 -0300

    Add OpenAIEmbeddings and FAISS vector store to app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
 
 
 def get_pdf_text(pdf_docs):
@@ -24,6 +26,12 @@ def get_text_chunks(text):
     return chunks
 
 
+def get_vector_store(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    vector_store = FAISS.from_texts(texts=text_chunks, embeddings=embeddings)
+    return vector_store
+
+
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with your documents", page_icon="📝")
@@ -42,13 +50,11 @@ def main():
             with st.spinner("Processing your documents..."):
                 # get pdf text
                 raw_text = get_pdf_text(pdf_docs)
-
-                st.write(raw_text)
-
                 # get pdf embeddings
                 text_chunks = get_text_chunks(raw_text)
 
-            # create vector store
+                # create vector store
+                vector_store = get_vector_store(text_chunks)
 
 
 if __name__ == "__main__":
